d0e43399a6f16216ae0d808e6b3199b377e85f0d
t <garte5484@gmail.com>
2026-08-27 12:16:30 +0300

    Delete rust directory

--- rust/build.rs
+++
@@ -1,8 +0,0 @@
-use cmake::Config;
-
-fn main() {
-    let dst = Config::new(".").build_target("byedpi").build();
-    let lib_path = dst.join("build");
-    println!("cargo:rustc-link-search=native={}", lib_path.display());
-    println!("cargo:rustc-link-lib=static=byedpi");
-}
